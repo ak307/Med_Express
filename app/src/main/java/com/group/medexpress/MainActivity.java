a9b388b5dc9c03cea6d859bcf88729be50ab1fe5
arkar <arkarminthant572@gmail.com>
2021-10-27 04:19:41 +0800

    finish creating bottom nav bar

--- app/src/main/java/com/group/medexpress/MainActivity.java
+++ app/src/main/java/com/group/medexpress/MainActivity.java
@@ -1,16 +1,61 @@
 package com.group.medexpress;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationview;
+    private Fragment homeFragment;
+    private Fragment meFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("dd");
+        bottomNavigationview = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+
+        homeFragment = new HomeFragment();
+        meFragment = new MeFragment();
+
+        //This is for automatically home page with Fragment when we open the app
+        replaceFragment(homeFragment);
+
+        setBottomNavigationView();
     }
+
+    private void setBottomNavigationView(){
+        bottomNavigationview.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.homeNav :
+                        replaceFragment(homeFragment);
+                        return true;
+                    case R.id.classroomNav:
+                        replaceFragment(meFragment);
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.mainContainer, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
