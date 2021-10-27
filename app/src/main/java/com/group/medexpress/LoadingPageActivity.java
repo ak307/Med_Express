845a42f7132191cebd246f8ec198b2d364312ce8
arkar <arkarminthant572@gmail.com>
2021-10-27 16:34:27 +0800

    finish creating Loading Page

---
+++ app/src/main/java/com/group/medexpress/LoadingPageActivity.java
@@ -0,0 +1,53 @@
+package com.group.medexpress;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class LoadingPageActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN_TIMEOUT = 3000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_page);
+
+        ImageView imageView = (ImageView) findViewById(R.id.splashImageView);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
+        TextView textView3 = (TextView) findViewById(R.id.textView3);
+        TextView textView4 = (TextView) findViewById(R.id.textView4);
+        TextView textView5 = (TextView) findViewById(R.id.textView5);
+
+
+        Animation fadeout = new AlphaAnimation(1, 0);
+        fadeout.setInterpolator(new AccelerateInterpolator());
+        fadeout.setStartOffset(500);
+        fadeout.setDuration(1800);
+        imageView.setAnimation(fadeout);
+        textView.setAnimation(fadeout);
+        textView2.setAnimation(fadeout);
+        textView3.setAnimation(fadeout);
+        textView4.setAnimation(fadeout);
+        textView5.setAnimation(fadeout);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LoadingPageActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+}
