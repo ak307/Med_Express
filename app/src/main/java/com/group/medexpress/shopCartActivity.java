d07a28a75247dbc2bb2f2771edf17c8744e9eaf4
Dong <979228466@QQ.com>
2021-11-16 16:53:35 +0800

    git git pull origin master

---
+++ app/src/main/java/com/group/medexpress/shopCartActivity.java
@@ -0,0 +1,14 @@
+package com.group.medexpress;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class shopCartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_cart);
+    }
+}
